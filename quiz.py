022c3b18e083652bf85ace127e389b32a08a4478
l0cky12 <73147961+l0cky12@users.noreply.github.com>
2021-07-01 06:48:07 -0500

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,19 @@
+score = 0
+
+q1 = input("what is windows 10?: ")
+q2 = input("fill in the plank ___/linux: ")
+
+
+if q1 == "os":
+    score += 1
+    print(score)
+else:
+    print("sorry you are worng")
+
+if q2 == "gnu":
+    score += 1
+    print(score)
+else:
+    print("what a fucking dumbass.")
+
+print(score)
